29cf8c55c9c3f45c820831af9bccc88b0f3c628c
Eric Laitman <elaitman1@gmail.com>
2019-07-01 16:27:38 -0700

    edit

--- src/components/StopWatch.js
+++ src/components/StopWatch.js
@@ -3,22 +3,22 @@ import styled from 'styled-components'
 
 class StopWatch extends React.Component{
 
-  // handleClick = (event) => {
-  //   let x = 0
-  //   while(x >= 0){
-  //     if(event.value.innerHTML === 'start'){
-  //       <div>
-  //       {x+=1}
-  //       </div>
-  //     }else{
-  //       <div>{x}</div>
-  //     }
-  //   }
-  // }
+  handleClick = (event) => {
+    let x = 0
+    while(x >= 0){
+      if(event.value.innerHTML === 'start'){
+        <div>
+        {x+=1}
+        </div>
+      }else{
+        <div>{x}</div>
+      }
+    }
+  }
 
   render() {
     return <>
-    // <h1>Stop Watch</h1>
+    <h1>Stop Watch</h1>
     <div>0ms</div>
     <br />
     <button>start</button>
